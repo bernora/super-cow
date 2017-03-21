e103fc109d94b4f1e4f19af4847b6a0f12ccdef6
Nathan Mahdavi <mail@nathanmahdavi.com>
2017-03-21 22:11:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+let totalSP = 0;
+
+function calculateSP(issue, i) {
+  totalSP = totalSP + issue.fields.customfield_10362;
+}
+
+data.issues.forEach(calculateSP);
+
+console.log(totalSP);
